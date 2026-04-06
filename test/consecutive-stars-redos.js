1b09bbff7bd253808a88ee970caa369f9eee9e72
Shaked Shauli <shaked.s@sealsecurity.io>
2026-04-06 19:50:03 +0300

    patched v3.0.3

---
+++ test/consecutive-stars-redos.js
@@ -0,0 +1,25 @@
+var t = require('tap')
+var Minimatch = require('../').Minimatch
+
+t.test('consecutive stars are coalesced', function (t) {
+  var re1 = new Minimatch('a*b').makeRe()
+  var re3 = new Minimatch('a***b').makeRe()
+  t.equal(re3.toString(), re1.toString(), 'a***b same regex as a*b')
+  t.end()
+})
+
+function repeat(str, n) {
+  return Array(n + 1).join(str)
+}
+
+t.test('100+ consecutive stars do not cause ReDoS', function (t) {
+  var stars = repeat('*', 100)
+  var pattern = 'a' + stars + 'b'
+  var start = Date.now()
+  var mm = new Minimatch(pattern)
+  var re = mm.makeRe()
+  re.test('a' + repeat('c', 25))
+  var elapsed = Date.now() - start
+  t.ok(elapsed < 1000, 'completed in ' + elapsed + 'ms (< 1s)')
+  t.end()
+})
